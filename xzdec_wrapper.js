5c63172d2c8a882798fad84d2354a2bd02a6bc86
Peter <peter@exinto.de>
2015-04-18 22:49:11 +0200

    Optimized xzdec build.

--- xzdec_wrapper.js
+++ xzdec_wrapper.js
@@ -2,16 +2,19 @@ var dec = require('./xzdec.js');
 dec._init();
 
 var utils = require('./utils.js');
-var memsize = 500000;
+var memsize = 2 * 1024 * 1024;
 var mem = dec._malloc(memsize);
 
 module.exports.decompress = utils.promisify(function(data, callback)
 {
     dec.writeArrayToMemory(data, mem);
     var err = dec._uncompress(mem, memsize);
-    var ptr = dec._getUncompressedBuffer();
-    var length = dec._getUncompressedLength();
-    //@todo make use of "err"
-    //@todo we should probably copy that
-    callback(null, dec.HEAP8.subarray(ptr, ptr + length));
+    if (err !== 1)
+        callback(err);
+    else
+    {
+        var ptr = dec._getUncompressedBuffer();
+        var length = dec._getUncompressedLength();
+        callback(null, dec.HEAP8.subarray(ptr, ptr + length));
+    }
 });
